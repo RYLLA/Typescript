6b6dc0531dc601c7a4c1c78e90da6dbff14497b5
rylla <stpv.milan@hotmail.com>
2025-09-16 19:22:49 -0500

    Variables y Constantes

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,10 @@
+
+const firstName: string = "illika";
+let lastName = "Nilo";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const constainsLetterK = firstName.includes('k');
+
+console.log({constainsLetterK, firstName, lastName});
